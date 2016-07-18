88c9a405f2e21ae9de5705f803adb79edbdbb9a6
etj <etj@geo-solutions.it>
2016-07-18 15:23:23 +0200

    Add info pages.

--- ckanext/odn/plugin_theme.py
+++ ckanext/odn/plugin_theme.py
@@ -1,13 +1,16 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import ckan.lib.base as base
+
 import ckanext.odn.helpers as helpers
 
 
 class OdnThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
-    
+    plugins.implements(plugins.IRoutes, inherit=True)
+
     # IConfigurer
 
     def update_config(self, config_):
@@ -15,13 +18,24 @@ class OdnThemePlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'odn')
 
-
     # ITemplateHelpers
     def get_helpers(self):
         return {'recent_updates': helpers.recent_updates,
                 'group_list': helpers.group_list,
                 'get_odn_config': helpers.get_odn_config}
 
+    # IRoutes
+    def before_map(self, map):
+        map.connect('istruzioni', '/istruzioni', controller='ckanext.odn.plugin_theme:ODNController', action='istruzioni')
+        map.connect('opendata', '/opendata', controller='ckanext.odn.plugin_theme:ODNController', action='opendata')
+
+        return map
 
 
+class ODNController(base.BaseController):
 
+    def istruzioni(self):
+        return base.render('home/odn_istruzioni.html')
+
+    def opendata(self):
+        return base.render('home/odn_opendata.html')
